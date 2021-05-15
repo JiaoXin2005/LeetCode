5cd8e13fe942245e62ac8e228fa2d5e238946324
jiaoxin <337471479@qq.com>
2021-05-15 13:59:02 +0800

    feat: 46

--- 46.全排列.js
+++ 46.全排列.js
@@ -9,9 +9,26 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    const element = nums[i];
-  }
+var permute = function(nums) {
+  const res = [];
+
+  const backtrack = (path, select) => {
+    if (path.length === select.length) {
+      res.push(path.slice(0));
+      return;
+    }
+
+    for (let i = 0; i < select.length; i++) {
+      let cur = select[i];
+      if (!path.includes(cur)) {
+        path.push(cur);
+        backtrack(path, select);
+        path.pop();
+      }
+    }
+  };
+
+  backtrack([], nums);
+  return res;
 };
 // @lc code=end
