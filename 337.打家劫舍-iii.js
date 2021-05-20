82b7c50d580503c7e9b47e4aa7ee7c06597e9623
jiaoxin <337471479@qq.com>
2021-05-20 14:55:12 +0800

    feat: 337

---
+++ 337.打家劫舍-iii.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=337 lang=javascript
+ *
+ * [337] 打家劫舍 III
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var rob = function(root) {
+  const map = new Map();
+  const dfs = (root) => {
+    if (!root) return 0;
+    if (map.has(root)) return map.get(root);
+
+    let baseVal = root.val;
+
+    if (root.left) {
+      baseVal += dfs(root.left.left) + dfs(root.left.right);
+    }
+
+    if (root.right) {
+      baseVal += dfs(root.right.left) + dfs(root.right.right);
+    }
+
+    let notRootVal = dfs(root.left) + dfs(root.right);
+
+    let res = Math.max(baseVal, notRootVal);
+
+    map.set(root, res);
+
+    return res;
+  };
+
+  return dfs(root);
+};
+// @lc code=end
+
+// [4,1,null,2,null,3]
+// [79,99,77,null,null,null,69,null,60,53,null,73,11,null,null,null,62,27,62,null,null,98,50,null,null,90,48,82,null,null,null,55,64,null,null,73,56,6,47,null,93,null,null,75,44,30,82,null,null,null,null,null,null,57,36,89,42,null,null,76,10,null,null,null,null,null,32,4,18,null,null,1,7,null,null,42,64,null,null,39,76,null,null,6,null,66,8,96,91,38,38,null,null,null,null,74,42,null,null,null,10,40,5,null,null,null,null,28,8,24,47,null,null,null,17,36,50,19,63,33,89,null,null,null,null,null,null,null,null,94,72,null,null,79,25,null,null,51,null,70,84,43,null,64,35,null,null,null,null,40,78,null,null,35,42,98,96,null,null,82,26,null,null,null,null,48,91,null,null,35,93,86,42,null,null,null,null,0,61,null,null,67,null,53,48,null,null,82,30,null,97,null,null,null,1,null,null]
